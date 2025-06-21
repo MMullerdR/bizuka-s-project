c7821adc83f227769d640449b4178ac0846e49af
MMullerdR <miguelmullerdarosa@gmail.com>
2025-06-21 14:32:37 -0300

    Atualização bizuka game para v0.03 - implemento de rodadas alternadas

--- bizuka.c
+++ bizuka.c
@@ -24,56 +24,147 @@ int main(){
 
         if(strcmp(menu,"jogar") == 0){
 
-            // Criar repeticao alternada entre jogador chutar primeiro e pc chutar primeiro:
-
-            // Rodada do jogador chutar primeiro --------------------------------------------------------------
-            printf("Digite seu numero:");
-            scanf("%d", &numerojogador);
-
-            while(numerojogador > jogmax || numerojogador < 0){
-                printf("Numero invalido, digite novamente:\n");
+                // Criar repeticao alternada entre jogador chutar primeiro e pc chutar primeiro:
+            for(int rodada = 1; rodada <=100; rodada++){
+                sleep(1.5);
+                printf("O jogador pode jogar de 0 a %d.\nO computador pode jogar de 0 a %d.\n\n", jogmax, npcmax);
+                sleep(3);
+            if(rodada % 2 == 1){
+// ----------------------------------------------------------------------------------------------------------------
+                // Rodada do jogador chutar primeiro --------------------------------------------------------------
+                printf("Voce sera o primeiro a tentar acertar a soma.\n");
+                printf("Digite seu numero:\n");
                 scanf("%d", &numerojogador);
-            }
-            printf("O computador vai pensar em um numero... \n");
-            numeropc = (rand() % (npcmax - npcmin + 1)) + npcmin; // escolhe um numero aleatorio entre 0 e 3 para o computador
 
-            sleep(1.5);
-            printf("Numero escolhido!\n");
-            sleep(1);
-            printf("Quanto vai dar a soma?\n");
-            scanf("%d", &chutesomajog); // pergunta para o jogador o chute para soma ---------------------
+                while(numerojogador > jogmax || numerojogador < 0){
+                    printf("Numero invalido, digite novamente:\n");
+                    scanf("%d", &numerojogador);
+                }
+                printf("O computador vai escolher um numero... \n");
+                numeropc = (rand() % (npcmax - npcmin + 1)) + npcmin; // escolhe um numero aleatorio entre 0 e 3 para o computador
 
-            supnumjog = (rand() % (jogmax + 1)); // gera um numero aleatorio que chuta qual o numero escolhido pelo jogador
-            chutesomapc = supnumjog + numeropc; // soma o numero escolhido pelo pc com o numero que ele acredita que o jogador escolheu
-        
-            while (chutesomapc == chutesomajog){
-                supnumjog = (rand() % (jogmax + 1));
-                chutesomapc = supnumjog + numeropc;
-            } // evita que o computador escolha um numero igual ao do jogador
+                sleep(1.5);
+                printf("Numero escolhido!\n");
+                sleep(1);
+                printf("Quanto vai dar a soma?\n");
+                scanf("%d", &chutesomajog); // pergunta para o jogador o chute para soma ---------------------
 
-            printf("O computador acha que a soma vai dar: %d\n", chutesomapc);
-            sleep(2);
-            printf("O computador escolheu: %d\n", numeropc);
-            sleep(2);
+                supnumjog = (rand() % (jogmax + 1)); // gera um numero aleatorio que chuta qual o numero escolhido pelo jogador
+                chutesomapc = supnumjog + numeropc; // soma o numero escolhido pelo pc com o numero que ele acredita que o jogador escolheu
+            
+                while (chutesomapc == chutesomajog){
+                    supnumjog = (rand() % (jogmax + 1));
+                    chutesomapc = supnumjog + numeropc;
+                } // evita que o computador escolha um numero igual ao do jogador
 
-            soma = numeropc + numerojogador;
+                printf("O computador acha que a soma vai dar: %d\n", chutesomapc);
+                sleep(2);
+                printf("O computador escolheu: %d\n", numeropc);
+                sleep(2);
 
-            if(soma==0){printf("Deu bizuka! (Soma = 0)\n");} // Mostra o resultado da soma
-            else{printf("A soma foi igual a: %d\n", soma);}
+                soma = numeropc + numerojogador;
 
-            sleep(2);
+                if(soma==0){printf("Deu bizuka! (Soma = 0)\n");} // Mostra o resultado da soma
+                else{printf("A soma foi igual a: %d\n", soma);}
 
-            if(chutesomajog==soma){
-                jogmax--;
-                printf("PARABENS! Voce marcou 1 ponto. Agora voce deve escolher um numero de 0 ate %d.\n", jogmax);
-            }
-            else if(chutesomapc==soma){
-                npcmax--;
-                printf(":(\nO computador acertou o chute. Agora ele pode escolher um numero de 0 ate %d.\n", npcmax);
-            }
+                sleep(2);
+
+                if(chutesomajog==soma){jogmax--;}
+                else if(chutesomapc==soma){npcmax--;}
+
+                if(jogmax != 0 && npcmax != 0){ // se nao for o fim do jogo, faça:
+                    if(chutesomajog==soma){
+                        printf("PARABENS! Voce marcou 1 ponto. Agora voce deve escolher um numero de 0 ate %d.\n\n", jogmax);
+                    }
+                    else if(chutesomapc==soma){
+                        printf(":(\nO computador acertou o chute. Agora ele pode escolher um numero de 0 ate %d.\n\n", npcmax);
+                    }
+                    else{
+                        printf("Ninguem pontuou, o jogo continua.\n\n");
+                    }
+                }
+                else if(jogmax == 0){
+                    printf("VOCE GANHOU O JOGO! BIZUQUEIRO GALO!");
+                    break;  // se for o fim do jogo jogador
+                }
+                else if(npcmax == 0){
+                    printf("Bah, tu e ruim pia. Perdeu pro computador.");
+                    break;  // se for o fim do jogo pc
+                } }
             // Rodada do jogador chutar primeiro --------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------
 
-            break; // finaliza o programa
+
+                if(rodada % 2 == 0){
+// ----------------------------------------------------------------------------------------------------------------
+                // Rodada do jogador chutar por segundo --------------------------------------------------------------
+                printf("Voce sera o segundo a tentar acertar a soma.\n");
+                printf("Digite seu numero:\n");
+                scanf("%d", &numerojogador);
+
+                while(numerojogador > jogmax || numerojogador < 0){
+                    printf("Numero invalido, digite novamente:\n");
+                    scanf("%d", &numerojogador);
+                }
+                printf("O computador vai escolher um numero... \n");
+                numeropc = (rand() % (npcmax - npcmin + 1)) + npcmin; // escolhe um numero aleatorio entre 0 e 3 para o computador
+
+                sleep(1.5);
+                printf("Numero escolhido!\n");
+                sleep(1);
+
+                supnumjog = (rand() % (jogmax + 1)); // gera um numero aleatorio que chuta qual o numero escolhido pelo jogador
+                chutesomapc = supnumjog + numeropc; // soma o numero escolhido pelo pc com o numero que ele acredita que o jogador escolheu
+                
+                printf("O computador acha que a soma vai dar: %d\n", chutesomapc);
+                sleep(2);
+
+                printf("Quanto vai dar a soma?\n");
+                scanf("%d", &chutesomajog); // pergunta para o jogador o chute para soma ---------------------
+
+                while (chutesomajog == chutesomapc){
+                    printf("Voce nao pode escolher o mesmo numero que o computador.\n Digite outro numero para soma:\n");
+                    scanf("%d", &chutesomajog);
+                } // evita que o jogador escolha um numero igual ao do pc
+                sleep(2);
+                printf("O computador escolheu: %d\n", numeropc);
+                sleep(2);
+
+                soma = numeropc + numerojogador;
+
+                if(soma==0){printf("Deu bizuka! (Soma = 0)\n");} // Mostra o resultado da soma
+                else{printf("A soma foi igual a: %d\n", soma);}
+
+                sleep(2);
+
+                if(chutesomajog==soma){jogmax--;}
+                else if(chutesomapc==soma){npcmax--;}
+
+                if(jogmax != 0 && npcmax != 0){ // se nao for o fim do jogo, faça:
+                    if(chutesomajog==soma){
+                        printf("PARABENS! Voce marcou 1 ponto. Agora voce deve escolher um numero de 0 ate %d.\n\n", jogmax);
+                    }
+                    else if(chutesomapc==soma){
+                        printf(":(\nO computador acertou o chute. Agora ele pode escolher um numero de 0 ate %d.\n\n", npcmax);
+                    }
+                    else{
+                        printf("Ninguem pontuou, o jogo continua.\n\n");
+                    }
+                }
+                else if(jogmax == 0){
+                    printf("VOCE GANHOU O JOGO! BIZUQUEIRO GALO!");
+                    break;  // se for o fim do jogo jogador
+                }
+                else if(npcmax == 0){
+                    printf("Bah, tu e ruim pia. Perdeu pro computador.");
+                    break;  // se for o fim do jogo pc
+                } }
+            // Rodada do jogador chutar por segundo --------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------
+
+            } // chave que fecha o "for" que alterna as rodadas do computador e do jogador;
+              
+
         }
 
         else if(strcmp(menu, "regras") == 0){
